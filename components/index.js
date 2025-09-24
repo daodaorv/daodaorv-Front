35561a8191ee4d4d56b70fd954653d403addd1bf
daodaorv <107552987+daodaorv@users.noreply.github.com>
2025-09-24 15:26:51 +0800

    同步组件库入口文件 - 包含88个自定义组件的注册和导出

---
+++ components/index.js
@@ -0,0 +1,292 @@
+// UI基础组件
+import DdButton from './ui/dd-button.vue'
+import DdIcon from './ui/dd-icon.vue'
+import DdImage from './ui/dd-image.vue'
+import DdLoading from './ui/dd-loading.vue'
+import DdBadge from './ui/dd-badge.vue'
+import DdTag from './ui/dd-tag.vue'
+import DdDivider from './ui/dd-divider.vue'
+import DdTable from './ui/dd-table.vue'
+import DdListItem from './ui/dd-list-item.vue'
+
+// 表单组件
+import DdInput from './form/dd-input.vue'
+import DdTextarea from './form/dd-textarea.vue'
+import DdRadio from './form/dd-radio.vue'
+import DdCheckbox from './form/dd-checkbox.vue'
+import DdSwitch from './form/dd-switch.vue'
+import DdPicker from './form/dd-picker.vue'
+import DdDatePicker from './form/dd-date-picker.vue'
+import DdRate from './form/dd-rate.vue'
+import DdSlider from './form/dd-slider.vue'
+import DdStepper from './form/dd-stepper.vue'
+import DdUpload from './form/dd-upload.vue'
+import DdForm from './form/dd-form.vue'
+import DdFormItem from './form/dd-form-item.vue'
+import DdSearch from './form/dd-search.vue'
+import DdSelect from './form/dd-select.vue'
+import DdRadioGroup from './form/dd-radio-group.vue'
+import DdCheckboxGroup from './form/dd-checkbox-group.vue'
+
+// 布局展示组件
+import DdCell from './layout/dd-cell.vue'
+import DdList from './layout/dd-list.vue'
+import DdCard from './layout/dd-card.vue'
+import DdCollapse from './layout/dd-collapse.vue'
+import DdTabs from './layout/dd-tabs.vue'
+import DdSwiper from './layout/dd-swiper.vue'
+import DdGrid from './layout/dd-grid.vue'
+import DdTimeline from './layout/dd-timeline.vue'
+import DdSteps from './layout/dd-steps.vue'
+import DdNoticeBar from './layout/dd-notice-bar.vue'
+import DdEmpty from './layout/dd-empty.vue'
+import DdAvatar from './layout/dd-avatar.vue'
+import DdHeader from './layout/dd-header.vue'
+import DdLoadMore from './layout/dd-load-more.vue'
+import DdTabBar from './layout/dd-tab-bar.vue'
+
+// 特殊组件
+import DdCalendar from './special/dd-calendar.vue'
+import DdCityPicker from './special/dd-city-picker.vue'
+import DdAddressPicker from './special/dd-address-picker.vue'
+import DdMapView from './special/dd-map-view.vue'
+import DdOrderTimeline from './special/dd-order-timeline.vue'
+import DdCalculator from './special/dd-calculator.vue'
+import DdChart from './special/dd-chart.vue'
+
+// 反馈组件
+import DdPopup from './functional/dd-popup.vue'
+import DdToast from './functional/dd-toast.vue'
+import DdNotify from './functional/dd-notify.vue'
+import DdDropdown from './functional/dd-dropdown.vue'
+import DdPopover from './functional/dd-popover.vue'
+import DdSkeleton from './functional/dd-skeleton.vue'
+import DdResult from './functional/dd-result.vue'
+import DdProgress from './functional/dd-progress.vue'
+import DdCountDown from './functional/dd-count-down.vue'
+import DdPullRefresh from './functional/dd-pull-refresh.vue'
+import DdBackTop from './functional/dd-back-top.vue'
+import DdOverlay from './functional/dd-overlay.vue'
+import DdModal from './functional/dd-modal.vue'
+import DdActionSheet from './functional/dd-action-sheet.vue'
+import DdDialog from './functional/dd-dialog.vue'
+import DdFilter from './functional/dd-filter.vue'
+import DdSignature from './functional/dd-signature.vue'
+
+// 业务组件
+import DdRvCard from './business/dd-rv-card.vue'
+import DdBookingCalendar from './business/dd-booking-calendar.vue'
+import DdPriceDisplay from './business/dd-price-display.vue'
+import DdUserInfo from './business/dd-user-info.vue'
+import DdCommentItem from './business/dd-comment-item.vue'
+import DdPaymentMethod from './business/dd-payment-method.vue'
+import DdLocationSelector from './business/dd-location-selector.vue'
+import DdRvFilter from './business/dd-rv-filter.vue'
+import DdBookingForm from './business/dd-booking-form.vue'
+import DdOrderCard from './business/dd-order-card.vue'
+import DdComment from './business/dd-comment.vue'
+import DdPostCard from './business/dd-post-card.vue'
+import DdRvParams from './business/dd-rv-params.vue'
+import DdCoupon from './business/dd-coupon.vue'
+import DdVehicleCard from './business/dd-vehicle-card.vue'
+import DdProductItem from './business/dd-product-item.vue'
+import BookingModule from './business/BookingModule.vue'
+
+// UI基础组件导出
+export { DdButton }
+export { DdIcon }
+export { DdImage }
+export { DdLoading }
+export { DdBadge }
+export { DdTag }
+export { DdDivider }
+export { DdTable }
+export { DdListItem }
+
+// 表单组件导出
+export { DdInput }
+export { DdTextarea }
+export { DdRadio }
+export { DdCheckbox }
+export { DdSwitch }
+export { DdPicker }
+export { DdDatePicker }
+export { DdRate }
+export { DdSlider }
+export { DdStepper }
+export { DdUpload }
+export { DdForm }
+export { DdFormItem }
+export { DdSearch }
+export { DdSelect }
+export { DdRadioGroup }
+export { DdCheckboxGroup }
+
+// 布局展示组件导出
+export { DdCell }
+export { DdList }
+export { DdCard }
+export { DdCollapse }
+export { DdTabs }
+export { DdSwiper }
+export { DdGrid }
+export { DdTimeline }
+export { DdSteps }
+export { DdNoticeBar }
+export { DdEmpty }
+export { DdAvatar }
+export { DdHeader }
+export { DdLoadMore }
+export { DdTabBar }
+
+// 特殊组件导出
+export { DdCalendar }
+export { DdCityPicker }
+export { DdAddressPicker }
+export { DdMapView }
+export { DdOrderTimeline }
+export { DdCalculator }
+export { DdChart }
+
+// 反馈组件导出
+export { DdPopup }
+export { DdToast }
+export { DdNotify }
+export { DdDropdown }
+export { DdPopover }
+export { DdSkeleton }
+export { DdResult }
+export { DdProgress }
+export { DdCountDown }
+export { DdPullRefresh }
+export { DdBackTop }
+export { DdOverlay }
+export { DdModal }
+export { DdActionSheet }
+export { DdDialog }
+export { DdFilter }
+export { DdSignature }
+
+// 业务组件导出
+export { DdRvCard }
+export { DdBookingCalendar }
+export { DdPriceDisplay }
+export { DdUserInfo }
+export { DdCommentItem }
+export { DdPaymentMethod }
+export { DdLocationSelector }
+export { DdRvFilter }
+export { DdBookingForm }
+export { DdOrderCard }
+export { DdComment }
+export { DdPostCard }
+export { DdRvParams }
+export { DdCoupon }
+export { DdVehicleCard }
+export { DdProductItem }
+export { BookingModule }
+
+// 批量导出
+const components = {
+  // UI基础组件
+  DdButton,
+  DdIcon,
+  DdImage,
+  DdLoading,
+  DdBadge,
+  DdTag,
+  DdDivider,
+  DdTable,
+  DdListItem,
+  // 表单组件
+  DdInput,
+  DdTextarea,
+  DdRadio,
+  DdCheckbox,
+  DdSwitch,
+  DdPicker,
+  DdDatePicker,
+  DdRate,
+  DdSlider,
+  DdStepper,
+  DdUpload,
+  DdForm,
+  DdFormItem,
+  DdSearch,
+  DdSelect,
+  DdRadioGroup,
+  DdCheckboxGroup,
+  // 布局展示组件
+  DdCell,
+  DdList,
+  DdCard,
+  DdCollapse,
+  DdTabs,
+  DdSwiper,
+  DdGrid,
+  DdTimeline,
+  DdSteps,
+  DdNoticeBar,
+  DdEmpty,
+  DdAvatar,
+  DdHeader,
+  DdLoadMore,
+  DdTabBar,
+  // 特殊组件
+  DdCalendar,
+  DdCityPicker,
+  DdAddressPicker,
+  DdMapView,
+  DdOrderTimeline,
+  DdCalculator,
+  DdChart,
+  // 反馈组件
+  DdPopup,
+  DdToast,
+  DdNotify,
+  DdDropdown,
+  DdPopover,
+  DdSkeleton,
+  DdResult,
+  DdProgress,
+  DdCountDown,
+  DdPullRefresh,
+  DdBackTop,
+  DdOverlay,
+  DdModal,
+  DdActionSheet,
+  DdDialog,
+  DdFilter,
+  DdSignature,
+  // 业务组件
+  DdRvCard,
+  DdBookingCalendar,
+  DdPriceDisplay,
+  DdUserInfo,
+  DdCommentItem,
+  DdPaymentMethod,
+  DdLocationSelector,
+  DdRvFilter,
+  DdBookingForm,
+  DdOrderCard,
+  DdComment,
+  DdPostCard,
+  DdRvParams,
+  DdCoupon,
+  DdVehicleCard,
+  DdProductItem,
+  BookingModule
+}
+
+// 安装函数
+const install = (Vue) => {
+  Object.keys(components).forEach(key => {
+    Vue.component(key, components[key])
+  })
+}
+
+// 导出安装函数和组件
+export default {
+  install,
+  ...components
+}
